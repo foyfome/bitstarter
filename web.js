1f46e071e7f246ac7710fc847d75885137883999
Hongseok Kim <foyfome@gmail.com>
2013-07-09 18:01:08 +0000

    Read 'Hello World' from index.html

--- web.js
+++ web.js
@@ -5,7 +5,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
   var fs = require('fs');
   var buffer = new Buffer(256);
-  response.send(buffer.toString('utf8', fs.readFileSync('./index.html')));
+  buffer = fs.readFileSybce('./index.html');
+  response.send(buffer.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
